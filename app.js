94247dadf89fd40a890f7447598082fa634f5ba2
Elif Seray Dönmez <elifseraydonmez@gmail.com>
2018-05-06 19:56:55 +0300

    maxage

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var routes = require('./api/routes');
 var port = process.env.PORT || 3000;
 
 app.set('superSecret', config.secret);
-app.use(express.static('static'));
+app.use(express.static('static', { maxAge: 86400000 }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
