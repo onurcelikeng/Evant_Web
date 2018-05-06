d6916316e852ecd3f55854de6fef588e74c6b01e
Elif Seray Dönmez <elifseraydonmez@gmail.com>
2018-05-06 20:00:55 +0300

    fix

--- api/controllers/categoryController.js
+++ api/controllers/categoryController.js
@@ -13,7 +13,7 @@ exports.categories = function (req, res) {
             message: '0 category found.'
         });
         
-        res.set({'Cache-Control': 'public, max-age = 36500', 'Expires': new Date(Date.now() + 2592000000).toUTCString()}).status(200).send({
+        res.set({'Cache-Control': 'no-cache'}).status(200).send({
             isSuccess: true,
             data: {
                 categories: categories
